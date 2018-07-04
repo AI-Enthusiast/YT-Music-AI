711a1ce7bc6f193b1d8c841ba004cecdfebd182f
Cormac <builderofthefuture@gmail.com>
2018-07-04 14:20:29 -0700

    Added try/exception statments to MHT & main

--- main.py
+++ main.py
@@ -55,38 +55,38 @@ if __name__ == "__main__":
                 print("\n YT_Bot.py has been Terminated...")
                 YT_Bot.doneConvertion()
                 break
-            elif com[0] == "v":  # if video
-                # try:
-                YT_Bot.doneConvertion()
-                YT_Bot.convertVid(com[1])
+            elif len(com) < 2 and str(com[0]) != "help":  # if there is not enough input
+                print("ERROR: Insufficient arguments. For help type 'help'")
+            elif com[0] == "v":  # if video v
+                try:
+                    YT_Bot.downloading()
+                    YT_Bot.convertVid(com[1])
+                except YT_Bot.youtube_dl.utils.PostProcessingError and YT_Bot.youtube_dl.utils.DownloadError as e:
+                    YT_Bot.error(e)
             elif com[0] == "p":  # if playlist p
-                # try:
-                YT_Bot.convertPlaylist(com[1])
-                YT_Bot.downloading()
-                # except Error as e:
-                # print("ERROR: " + e)
+                try:
+                    YT_Bot.downloading()
+                    YT_Bot.convertPlaylist(com[1])
+                except YT_Bot.youtube_dl.utils.PostProcessingError and YT_Bot.youtube_dl.utils.DownloadError as e:
+                    YT_Bot.error(e)
             elif com[0] == "c":  # if channel c
-                # try:
-                YT_Bot.convertChannel(com[1])
-                YT_Bot.downloading()
-                # except Error as e:
-                # print("ERROR: " + e)
+                try:
+                    YT_Bot.downloading()
+                    YT_Bot.convertChannel(com[1])
+                except YT_Bot.youtube_dl.utils.PostProcessingError and YT_Bot.youtube_dl.utils.DownloadError as e:
+                    YT_Bot.error(e)
             elif com[0] == "s":  # if search s
                 argparser.add_argument("--q", help="Search term", default=path[1])
                 argparser.add_argument("--max-results", help="Max results", default=25)
                 args = argparser.parse_args()
+
                 argparser.set_defaults(q=com[1])
                 args = argparser.parse_args()
                 try:
                     YT_Bot.youtube_search(args)
                 except HttpError as e:
                     print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
-            elif com[0] == "updateCSV":  # if updateCSV
-                MusicHashTable.updateCSV()  # if wanting to run tests
-            elif com[0] == "runTests":
-                MusicHashTable.runTests()
-            elif com[0] == "clear":  # If wanting to clear csv
-                MusicHashTable.clear()
+
             # elif com[0] == "auto":
             # enter automatic stage
             else:
