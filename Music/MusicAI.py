54dfb2e382e3dd7d4512a122307c389a9b17522f
Cormac <Rareear97>
2018-07-18 12:09:52 -0700

    Alpha for musicAi ready for testing

--- Music/MusicAI.py
+++ Music/MusicAI.py
@@ -1,4 +1,6 @@
 # Convolutional DNN
+from sklearn import tree
+
 from Music import MusicHashTable as mht
 
 user = mht.User('', '')
@@ -11,11 +13,21 @@ TestMusicPath = user.TestPath
 
 
 def label(data):
-    lables = []
+    labels = []
     for i in range(1, data.__len__()):
         entry = (str(data[i]).split(','))
-        mht.printRows(entry)
+        mht.printRows(entry)  # we want the ratios to get an initial idea on the videos performance
+        labels.append(entry[7:])
+    return labels
 
 
-data = mht.readData()
-lables = label()
+features = mht.readData()
+labels = label()
+
+# we are going to train a simple decition tree baised on the statistics rather than the music itself as an alpha ai
+clf = tree.DecisionTreeClassifir()
+clf = clf.fit(features, labels)
+print(clf.prdict['Flight of the Conchords', 'Robots', 'BNC61-OOPdA', 4100, 59, 559964, False, .9858, 69.49, .0073])
+
+# the next step will be to analize the music and understand that in the context of the ratios
+# then select similar sounding music that will have high performing ratios
